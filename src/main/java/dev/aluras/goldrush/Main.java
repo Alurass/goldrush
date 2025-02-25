5668fa34d3c00451d605dd10df8131cf55d38696
vearen <vearen@outlook.com>
2025-02-25 17:51:10 +0100

    feat: block breaking

--- src/main/java/dev/aluras/goldrush/Main.java
+++ src/main/java/dev/aluras/goldrush/Main.java
@@ -1,11 +1,9 @@
-package org.example;
+package dev.aluras.goldrush;
 
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
 import net.minestom.server.MinecraftServer;
 import net.minestom.server.coordinate.Pos;
-import net.minestom.server.coordinate.Vec;
-import net.minestom.server.entity.ItemEntity;
 import net.minestom.server.entity.Player;
 import net.minestom.server.event.GlobalEventHandler;
 import net.minestom.server.event.player.AsyncPlayerConfigurationEvent;
@@ -18,20 +16,15 @@ import net.minestom.server.instance.LightingChunk;
 import net.minestom.server.instance.block.Block;
 import net.minestom.server.item.ItemStack;
 import net.minestom.server.item.Material;
-
-import java.time.Duration;
-import java.util.concurrent.ThreadLocalRandom;
+import net.minestom.server.timer.TaskSchedule;
 
 public class Main {
-    public static void main(String[] args) {
-
-
-        MinecraftServer server = MinecraftServer.init();
+    public static void main(final String[] args) {
+        final MinecraftServer server = MinecraftServer.init();
 
         //Create an instance (a world)
-        InstanceManager instanceManager = MinecraftServer.getInstanceManager();
-        InstanceContainer instanceContainer = instanceManager.createInstanceContainer();
-
+        final InstanceManager instanceManager = MinecraftServer.getInstanceManager();
+        final InstanceContainer instanceContainer = instanceManager.createInstanceContainer();
 
         //generate the world
         instanceContainer.setGenerator(unit -> {
@@ -40,44 +33,40 @@ public class Main {
         //light world
         instanceContainer.setChunkSupplier(LightingChunk::new);
 
-        GlobalEventHandler globalEventHandler = MinecraftServer.getGlobalEventHandler();
+        final GlobalEventHandler handler = MinecraftServer.getGlobalEventHandler();
 
         //add an event handler to player spawning
-        globalEventHandler.addListener(AsyncPlayerConfigurationEvent.class, event -> {
+        handler.addListener(AsyncPlayerConfigurationEvent.class, event -> {
             final Player player = event.getPlayer();
             event.setSpawningInstance(instanceContainer);
             player.setRespawnPoint(new Pos(0, 42, 0));
         });
         //place block event
-        globalEventHandler.addListener(PlayerBlockPlaceEvent.class, event -> {
+        handler.addListener(PlayerBlockPlaceEvent.class, event -> {
             event.setCancelled(false);
         });
 
-
         //give player pickaxe
-        globalEventHandler.addListener(PlayerSpawnEvent.class, event -> {
-            Player player = event.getPlayer();
+        handler.addListener(PlayerSpawnEvent.class, event -> {
+            final Player player = event.getPlayer();
             player.getInventory().addItemStack(ItemStack.of(Material.IRON_PICKAXE));
             player.getInventory().addItemStack(ItemStack.of(Material.GOLD_ORE));
         });
 
-
         //gold ore block break event
-        globalEventHandler.addListener(PlayerBlockBreakEvent.class, event -> {
+        handler.addListener(PlayerBlockBreakEvent.class, event -> {
             if (event.getBlock() == Block.GOLD_ORE) {
-                Player player = event.getPlayer();
+                final Player player = event.getPlayer();
 
-                event.getInstance().setBlock(event.getBlockPosition(), Block.BEDROCK);
+                event.setResultBlock(Block.BEDROCK);
+                MinecraftServer.getSchedulerManager().scheduleTask(() -> event.getInstance().setBlock(event.getBlockPosition(), Block.GOLD_ORE), TaskSchedule.seconds(5), TaskSchedule.stop()); //epic block respawn moment
+
                 player.getInventory().addItemStack(ItemStack.of(Material.RAW_GOLD));
-
-                var message = Component.text("You mined a gold ore and earned", NamedTextColor.WHITE)
+                player.sendMessage(Component.text("You mined a gold ore and earned", NamedTextColor.WHITE)
                         .append(Component.text(" 1 coin", NamedTextColor.GOLD)
-                                .append(Component.text("!", NamedTextColor.WHITE)));
-
-                event.getPlayer().sendMessage(message);
+                                .append(Component.text("!", NamedTextColor.WHITE))));
             }
         });
-
 
         server.start("0.0.0.0", 25565);
     }
