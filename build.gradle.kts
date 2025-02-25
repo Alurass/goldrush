216c9004258d30f7e38842ee3ebab9ac93cd53f9
vearen <vearen@outlook.com>
2025-02-25 17:50:42 +0100

    chore: gradle.properties

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,19 +2,13 @@ plugins {
     id("java")
 }
 
-group = "org.example"
-version = "1.0-SNAPSHOT"
+group = "dev.aluras"
+version = "1.0"
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-    implementation("net.minestom:minestom-snapshots:620ebe5d6b")
-}
-
-tasks.test {
-    useJUnitPlatform()
+    implementation("net.minestom:minestom-snapshots:${project.property("minestomVersion")}")
 }
